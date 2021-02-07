1ab220572dd6109e9aa4e64b878dce19c373b143
Sarah French <sarah@sarahfrench.co.uk>
2021-02-07 17:12:25 +0000

    Started GuessedWords tests, half implemented

---
+++ src/GuessedWords.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const GuessedWords = ({ guessedWords }) => {
+  let contents;
+  if (guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default GuessedWords;
