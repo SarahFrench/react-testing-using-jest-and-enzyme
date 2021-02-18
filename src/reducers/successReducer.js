bc7a358007f02608516adb43aa18165c4a99e4a0
Sarah French <sarah@sarahfrench.co.uk>
2021-02-18 22:45:09 +0000

    Add tests for successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,12 @@
-export default (state, action) => {
-  return null;
+//take current state and action, return new state depending on action
+//state could be null if no initial state returned by a reducer yet on app start??
+import { actionTypes } from "../actions";
+
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return false;
+  }
 };
