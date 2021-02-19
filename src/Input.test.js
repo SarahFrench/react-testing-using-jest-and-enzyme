56e371d89f7a3842b2f9a9d4dcd021ef025f0a3c
Sarah French <sarah@sarahfrench.co.uk>
2021-02-19 00:03:14 +0000

    Write tests looking for elements when no words guessed

--- src/Input.test.js
+++ src/Input.test.js
@@ -6,15 +6,34 @@ import Input from "./Input";
 //Could not find "store" in the context of "Connect(Input)". Either wrap the root component in a <Provider>, or pass a custom React context provider to <Provider> and the corresponding React context consumer to Connect(Input) in connect options.
 
 const setup = (initialState = {}) => {
-  const wrapper = shallow(<Input store={storeFactory()} />);
-  console.log(wrapper.debug());
+  const wrapper = shallow(<Input store={storeFactory()} />)
+    .dive()
+    .dive();
+  return wrapper;
 };
+
 setup();
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("renders component without error", () => {});
-    test("renders input box", () => {});
-    test("renders submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {
+        success: false,
+      };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test("renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
   });
   describe("word has been guessed", () => {
     test("renders component without error", () => {});
