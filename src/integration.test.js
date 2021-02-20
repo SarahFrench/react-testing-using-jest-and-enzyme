25fb0c6755dfc22ee36996967291a0cff1a3be79
Sarah French <sarah@sarahfrench.co.uk>
2021-02-20 23:52:42 +0000

    Create guessed words reducer and begin making the integration test file for the new AC + reducer

---
+++ src/integration.test.js
@@ -0,0 +1,17 @@
+/*
+File used to test interaction between action creators and reducers
+*/
+
+import { storeFactory } from "../test/testUtils";
+import { guessWord } from "./actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    test("updates state correctly for unsuccessful guess", () => {});
+    test("updates state correctly for successful guess", () => {});
+  });
+  describe("no guessed words", () => {
+    test("updates state correctly for unsuccessful guess", () => {});
+    test("updates state correctly for successful guess", () => {});
+  });
+});
