9718fc9f65e70ab2548558d0f8b7cc4334d80f01
Sarah French <sarah@sarahfrench.co.uk>
2021-02-22 22:49:20 +0000

    Convert App component to class based so Enzyme works, connect to redux store and give access to action creators

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
 
+import { getSecretWord, guessWord } from "./actions";
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
 
-const App = () => {
-  return (
-    <div className="App container">
-      <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords
-        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-      />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
 };
 
-export default App;
+export default connect(mapStateToProps, { getSecretWord, guessWord })(App);
