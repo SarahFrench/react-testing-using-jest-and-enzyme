82606e36b8744242eb331f70f0d90df0a86e889a
Sarah French <sarah@sarahfrench.co.uk>
2021-02-22 19:29:37 +0000

    Convert Input component to a class based component

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Input = ({ success }) => {
-  const contents = success ? null : (
-    <form className="form-inline">
-      <input
-        data-test="input-box"
-        className="mb-2 mx-sm-3"
-        type="text"
-        placeholder="enter guess"
-      />
-      <button
-        data-test="submit-button"
-        className="btn btn-primary mb-2"
-        type="submit"
-      >
-        Submit
-      </button>
-    </form>
-  );
-  return <div data-test="component-input">{contents}</div>;
-};
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    );
+    return <div data-test="component-input">{contents}</div>;
+  }
+}
 
 const mapStateToProps = ({ success }) => {
   return { success };
