670e2a2ff3937e1e144ce6c6b8f749ef2fa8eeed
Sarah French <sarah@sarahfrench.co.uk>
2021-03-07 18:29:23 +0000

    Change store to allow use of Redux devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,25 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
+/**
+ * Old version of creating the Redux store
+ */
+
+// export const middlewares = [ReduxThunk];
+// const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+// export default createStoreWithMiddleware(rootReducer);
+
+/**
+ * New version, including code to use Redux dev tools
+ */
+
 export const middlewares = [ReduxThunk];
-const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
-export default createStoreWithMiddleware(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+export default store;
